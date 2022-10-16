641630d1cb7650148ead2ce77c3aceb029842f79
binburaka <ushindiburaka@gmail.com>
2022-10-16 16:59:51 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n < 58; n++)
+	for (a = 0; a < 100; a++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(a + '0');
+		if (a < 99)
 		{
 			putchar(',');
-			putchat(' ');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
