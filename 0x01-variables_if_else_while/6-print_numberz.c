d774535f02eb856a3f00e3592bbf35cabdb32bf3
binburaka <ushindiburaka@gmail.com>
2022-10-16 16:38:01 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,14 +4,16 @@
  * main - prints singlt digits in base 10
  * Return: Always 0
  */
-int main(0)
+int main(void)
 {
-	int num;
+	int c;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
 	putchar('\n');
-
 	return (0);
 }
