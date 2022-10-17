1db295faff5cbaf3fe2b2ad756a4324b48e9cfcb
binburaka <ushindiburaka@gmail.com>
2022-10-17 08:15:05 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+	int i, j;
 
-	for (num1 = 0; num1 <= 98; num++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
+			if (i == 98 && j == 99)
 				continue;
 
 			putchar(',');
