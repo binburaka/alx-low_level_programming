e4f540f437a21d3b8287d8aa56e33e16b174a874
binburaka <ushindiburaka@gmail.com>
2022-10-19 13:22:28 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,16 +13,16 @@ int main(void)
 
 	for (count = 0; count < 50; count++)
 	{
-		sum = fin1 + fib2;
+		sum = fib1 + fib2;
 		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
 
 		if (count == 49)
-			print('\n');
+			printf('\n');
 		else
-			print(", ");
+			printf(", ");
 	}
 
 	return (0);
