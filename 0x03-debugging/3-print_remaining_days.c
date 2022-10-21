7cdd60f1ba0a648297b3abd03f0b21f3f5faa0d7
binburaka <ushindiburaka@gmail.com>
2022-10-21 11:12:10 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,12 +11,9 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	month >= 2;
-	day >= 31;
-
-	if ((year % 4 == 0 || year % 400 == 0) || (year % 400 == 0 && (year % 10 == 0)))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
