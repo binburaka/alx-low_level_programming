b26ac91592ba266edcdc542bf25cd9ecabf6e22a
binburaka <ushindiburaka@gmail.com>
2022-11-01 14:06:57 -0700

    7 even more pointers abd arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies @n bytes from the memory area pointed
+ *	     to by @src into that pointed to by @dest.
+ *@dest: A pointer to the memory area to copy @src into.
+ *@n: the number of bytes to copy from @src.
+ *@src: the source buffer to copy characters from.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
