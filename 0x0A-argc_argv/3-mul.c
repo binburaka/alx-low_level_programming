e3baf3c45c7616b0fa685fb8a062ecc2c6744522
binburaka <ushindiburaka@gmail.com>
2022-11-07 06:14:33 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * main - prints the multiplication of two integers
+ * @argc: arguments counted
+ * @argv: argument vector
+ * Return : 0 if true, 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+
+		printf("%d\n", x * y);
+		return (0);
+	}
+
+	printf("Error\n");
+	return (1);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to be printed
+ * Return: 1 on success
+ * on error, -1 is returned, and set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
